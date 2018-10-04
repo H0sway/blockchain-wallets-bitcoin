1ef339d46cc31567614cdee7df22c6d74215bfec
Joshua Russell <jkrussell756@gmail.com>
2018-10-03 23:21:11 -0400

    configured routes

---
+++ routes/blockchain-routes.js
@@ -0,0 +1,9 @@
+// Handles the calls to the blockchain API.
+const express = require('express');
+const blockchainRouter = express.Router();
+const blockchainController = require('../controllers/blockchain-controller');
+
+// Route to the only call we're making now. Sends the single wallet information to the frontend.
+blockchainRouter.get('/', blockchainController.search);
+
+module.exports = blockchainRouter;
