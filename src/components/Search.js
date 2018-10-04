01339e9119d960c94ba8ae58ec4185d6196ca57a
Joshua Russell <jkrussell756@gmail.com>
2018-10-03 22:48:34 -0400

    Working on the search function, having a CORS issue. May solve later.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
-// Import react, bootstrap components
+// Import react, axios, bootstrap components
 import React, { Component } from 'react';
 import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
+import axios from 'axios';
+
 
 
 class Search extends Component {
@@ -8,12 +10,16 @@ class Search extends Component {
     super();
     this.state = {
       walletAddress: "",
+      dataLoading: false,
+      dataLoaded: false,
+      transactions: [],
+      error: false
     }
     this.handleChange = this.handleChange.bind(this);
+    this.searchBlockchain = this.searchBlockchain.bind(this);
   }
   validateAddress() {
     const length = this.state.walletAddress.length;
-    console.log(length);
     if (length < 36 && length > 25) return 'success';
     if (length > 35) return 'error';
     if (length < 26) return 'error';
@@ -24,10 +30,34 @@ class Search extends Component {
       walletAddress: e.target.value,
     })
   }
+  searchBlockchain(e) {
+    e.preventDefault()
+    const length = this.state.walletAddress.length;
+    if (length < 36 && length > 25) {
+      const address = this.state.walletAddress;
+      this.setState({
+        dataLoading: true,
+      })
+      axios({
+        method: 'GET',
+        headers: {"Access-Control-Allow-Origin": "localhost:8080"},
+        url: `https://blockchain.info/rawaddr/${address}?cors=true`
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+    else {
+      alert("Invalid bitcoin address");
+    }
+  }
   render() {
     return (
       <div className="Search">
-        <form>
+        <form onSubmit={this.searchBlockchain}>
           <FormGroup validationState={this.validateAddress()}>
             <ControlLabel>Wallet Address</ControlLabel>
             <FormControl
