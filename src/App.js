f8642f3b0cef8d68e76d070bacc14a25256b32b7
Joshua Russell <jkrussell756@gmail.com>
2018-10-09 12:29:09 -0400

    not bothering with routes now, more important things to focus on

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 // Import dependencies
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Import components
 import Header from './components/Header';
 import Search from './components/Search';
-import About from './components/About';
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <div className="App">
           <Header />
-          <Route exact path="/" component={Search} />
-          <Route exact path="/about" component={About} />
+          <Search />
         </div>
-      </Router>
     )
   }
 };
