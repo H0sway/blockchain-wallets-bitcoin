859229636289b526437e53c4deee59a5514ae0e0
Joshua Russell <jkrussell756@gmail.com>
2018-10-24 13:29:50 -0400

    Added the About component (no content), added routes to the header.

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+// Import react and bootstrap components
+import React, { Component } from 'react';
+
+class About extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="About">
+
+      </div>
+    )
+  }
+};
+
+export default About;
